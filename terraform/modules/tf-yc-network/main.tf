0ed73d7434d75500c88c2d3fd2d018a69a0f51bb
Ivan <ivan@192.168.1.5>
2022-12-05 18:10:06 +0300

    fixup! add tf conf

--- terraform/modules/tf-yc-network/main.tf
+++ terraform/modules/tf-yc-network/main.tf
@@ -5,5 +5,6 @@ data "yandex_vpc_network" "default" {
 
 #вытаскиваем id подсети (которая относится к сети default) в указанной зоне
 data "yandex_vpc_subnet" "default" {
+  for_each  = toset(var.zone_list)
   name = "${data.yandex_vpc_network.default.name}-${var.zone_list}"
 }
